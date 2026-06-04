419e9ac55b8ed6a7b1c78c4b28c1c9bc186cc378
Vishnu Haridasan <vishnuharidasan50@gmail.com>
2026-06-04 19:07:21 +0530

    canvas and display function

--- main.c
+++ main.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
-#
+#include <stdlib.h>
+#define ROWS 40
+#define COLS 80
+
+char canvas[ROWS][COLS];
+
+void clearCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
