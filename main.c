52764345584f84b6b8f441db7bf9de869d2ab1ea
Vishnu Haridasan <vishnuharidasan50@gmail.com>
2026-06-04 19:17:42 +0530

    canvas and display function

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS 40
-#define COLS 80
+
+#define ROWS 30
+#define COLS 60
+#define MAX_OBJECTS 100
 
 char canvas[ROWS][COLS];
+
+typedef struct
+{
+    int type;
+
+    int x1, y1;
+    int x2, y2;
+
+    int width;
+    int height;
+
+    int size;
+    int radius;
+
+} Shape;
+
+Shape objects[MAX_OBJECTS];
+int objectCount = 0;
 
 void clearCanvas()
 {
@@ -21,6 +41,8 @@ void clearCanvas()
 void displayCanvas()
 {
     int i, j;
+
+    printf("\n");
 
     for(i = 0; i < ROWS; i++)
     {
