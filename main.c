aabccc556954183f2e6e2e5dc611f5e5b8d7c8b2
Vishnu Haridasan <vishnuharidasan50@gmail.com>
2026-06-05 20:30:10 +0530

    canvas and display function, putPixel anddrawline function

--- main.c
+++ main.c
@@ -53,3 +53,45 @@ void displayCanvas()
         printf("\n");
     }
 }
+void putPixel(int x, int y)
+{
+    if(x >= 0 && x < ROWS && y >= 0 && y < COLS)
+    {
+        canvas[x][y] = '*';
+    }
+}
+
+/* ---------- Drawing Functions ---------- */
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+
+    int err = dx - dy;
+
+    while(1)
+    {
+        putPixel(x1, y1);
+
+        if(x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if(e2 > -dy)
+        {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if(e2 < dx)
+        {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
