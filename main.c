32f46ef7fad400776e1fd952de259f8678010b73
Vishnu Haridasan <vishnuharidasan50@gmail.com>
2026-06-08 20:05:18 +0530

    canvas and display function, putPixel and drawline function,draw triangle,rect,circle,redraw canvas, addshape function

--- main.c
+++ main.c
@@ -95,3 +95,161 @@ void drawLine(int x1, int y1, int x2, int y2)
         }
     }
 }
+void drawRectangle(int x, int y, int width, int height)
+{
+    int i;
+
+    for(i = 0; i < width; i++)
+    {
+        putPixel(x, y + i);
+        putPixel(x + height - 1, y + i);
+    }
+
+    for(i = 0; i < height; i++)
+    {
+        putPixel(x + i, y);
+        putPixel(x + i, y + width - 1);
+    }
+}
+
+void drawTriangle(int x, int y, int size)
+{
+    drawLine(x, y, x + size, y - size);
+    drawLine(x, y, x + size, y + size);
+    drawLine(x + size, y - size, x + size, y + size);
+}
+
+void drawCircle(int xc, int yc, int r)
+{
+    int x = 0;
+    int y = r;
+    int d = 3 - 2 * r;
+
+    while(x <= y)
+    {
+        putPixel(xc + x, yc + y);
+        putPixel(xc - x, yc + y);
+        putPixel(xc + x, yc - y);
+        putPixel(xc - x, yc - y);
+
+        putPixel(xc + y, yc + x);
+        putPixel(xc - y, yc + x);
+        putPixel(xc + y, yc - x);
+        putPixel(xc - y, yc - x);
+
+        if(d < 0)
+        {
+            d = d + 4 * x + 6;
+        }
+        else
+        {
+            d = d + 4 * (x - y) + 10;
+            y--;
+        }
+
+        x++;
+    }
+}
+void redrawCanvas()
+{
+    int i;
+
+    clearCanvas();
+
+    for(i = 0; i < objectCount; i++)
+    {
+        switch(objects[i].type)
+        {
+            case 1:
+                drawLine(
+                    objects[i].x1,
+                    objects[i].y1,
+                    objects[i].x2,
+                    objects[i].y2
+                );
+                break;
+
+            case 2:
+                drawRectangle(
+                    objects[i].x1,
+                    objects[i].y1,
+                    objects[i].width,
+                    objects[i].height
+                );
+                break;
+
+            case 3:
+                drawTriangle(
+                    objects[i].x1,
+                    objects[i].y1,
+                    objects[i].size
+                );
+                break;
+
+            case 4:
+                drawCircle(
+                    objects[i].x1,
+                    objects[i].y1,
+                    objects[i].radius
+                );
+                break;
+        }
+    }
+}
+
+/* ---------- Add Shape ---------- */
+
+void addShape()
+{
+    Shape s;
+
+    printf("\n1. Line\n");
+    printf("2. Rectangle\n");
+    printf("3. Triangle\n");
+    printf("4. Circle\n");
+
+    printf("Enter shape type: ");
+    scanf("%d", &s.type);
+
+    switch(s.type)
+    {
+        case 1:
+            printf("Enter x1 y1 x2 y2: ");
+            scanf("%d%d%d%d",
+                  &s.x1,&s.y1,
+                  &s.x2,&s.y2);
+            break;
+
+        case 2:
+            printf("Enter top row and column: ");
+            scanf("%d%d",&s.x1,&s.y1);
+
+            printf("Enter width and height: ");
+            scanf("%d%d",&s.width,&s.height);
+            break;
+
+        case 3:
+            printf("Enter top point (row col): ");
+            scanf("%d%d",&s.x1,&s.y1);
+
+            printf("Enter size: ");
+            scanf("%d",&s.size);
+            break;
+
+        case 4:
+            printf("Enter center row and column: ");
+            scanf("%d%d",&s.x1,&s.y1);
+
+            printf("Enter radius: ");
+            scanf("%d",&s.radius);
+            break;
+
+        default:
+            printf("Invalid shape.\n");
+            return;
+    }
+
+    objects[objectCount++] = s;
+
+    redrawCanvas();
+}
